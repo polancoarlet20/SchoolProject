ac05dc55b5b9c8176b7483e9267645f84cd9a59b
polancoarlet20 <polancoarlet20@gmail.com>
2025-03-08 07:54:17 +0000

    Add 3.js

---
+++ src/3.js
@@ -0,0 +1,24 @@
+// Return the sum of two numbers
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+// Return the difference between two numbers
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+// Return the product of two numbers
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+// Return the quotient of two numbers
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+// Return the remainder of two numbers
+function modulo(num1, num2) {
+  return num1 % num2;
+}
