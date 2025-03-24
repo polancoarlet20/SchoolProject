e75b531480d15229185efff615476772734a4363
polancoarlet20 <polancoarlet20@gmail.com>
2025-03-24 00:04:00 +0000

    Add 18.js

---
+++ src/18.js
@@ -0,0 +1,7 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+// Example usage:
+let randomNumber = getRandomInt(10);
+console.log(`Random number: ${randomNumber}`);
