6826182510395087d82b3f19216c51e3c67e5cca
polancoarlet20 <polancoarlet20@gmail.com>
2025-05-19 23:35:51 +0000

    Add 63.js

---
+++ src/63.js
@@ -0,0 +1,7 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+// Example usage:
+const result = getRandomInt(10); // Generates a random integer between 0 and 9
+console.log(result);
